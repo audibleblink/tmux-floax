c8705dd4f4222eb19d5b3b5ea3f7aad5d5e89c72
Alex Flores <domain@me.com>
2024-07-25 15:14:41 -0500

    stuff custom to me

--- scripts/floax.sh
+++ scripts/floax.sh
@@ -19,14 +19,17 @@ if [ "$(tmux display-message -p '#{session_name}')" = "$FLOAX_SESSION_NAME" ]; t
     tmux setenv -g FLOAX_TITLE "$FLOAX_TITLE"
     tmux detach-client
 else
-    # Check if the session 'scratch' exists
+    # Check if the session '"$FLOAX_SESSION_NAME"' exists
     if tmux has-session -t "$FLOAX_SESSION_NAME" 2>/dev/null; then
         set_bindings
         tmux_popup
     else
-        # Create a new session named 'scratch' and attach to it
+        # Create a new session named '"$FLOAX_SESSION_NAME"' and attach to it
         tmux new-session -d -c "$(tmux display-message -p '#{pane_current_path}')" -s "$FLOAX_SESSION_NAME"
         tmux set-option -t "$FLOAX_SESSION_NAME" status off
+        tmux set-option -t "$FLOAX_SESSION_NAME" status-position 'bottom'
+        tmux set-option -t "$FLOAX_SESSION_NAME" status on
+        tmux set-option -t "$FLOAX_SESSION_NAME" status-right ''
         tmux_popup
     fi
 fi
